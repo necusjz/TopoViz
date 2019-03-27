77bc81aecf9653c4c04eb3ead382bd19502c89dc
Xin Liu <xin.liu@thoughtworks.com>
2019-03-27 18:04:38 +0800

    update

--- FrontEnd/src/router.ts
+++ FrontEnd/src/router.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 import AppMain from './views/AppMain.vue';
 
 Vue.use(Router);
@@ -13,14 +12,6 @@ export default new Router({
       path: '/',
       name: 'AppMain',
       component: AppMain,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
   ],
 });
