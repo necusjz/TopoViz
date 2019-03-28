b189c86ea88658a7e118d595c1ead232b5bd4362
Xin Liu <xin.liu@thoughtworks.com>
2019-03-28 09:48:45 +0800

    update front-end

--- backend/view.py
+++ backend/view.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, redirect
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates', static_url_path='')
 
 # read config file
 app.config.from_object('config')
@@ -11,7 +11,7 @@ app.config.from_object('config')
 # upload excel file
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return redirect('/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
