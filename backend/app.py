3d50700e330b55a944f53260c6d49a66622389bd
umarellyh <umarellyh@gmail.com>
2019-03-29 22:52:04 +0800

    for weekend - hyang

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request
-# 解决跨域问题
-from flask_cors import CORS
+import os
+import pandas as pd
 import json
+import json2html
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug import secure_filename
+from flask_cors import CORS
 
-app = Flask(__name__, static_folder='static', static_url_path='')
-
-# read config file
+app = Flask(__name__)
 app.config.from_object('config')
-
 CORS(app, resources=r'/*')
 
-# return homepage
-@app.route('/', methods=['GET'])
-def index():
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file1 = request.files('file1')
+        file2 = request.files('file2')
+        date = request.form.get('date')
+        if file1 and file2 and date and allowed_file(file1.filename) and allowed_file(file2.filename):
+            filename1 = secure_filename(file1.filename)
+            filename2 = secure_filename(file2.filename)
+            file1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
+            file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
+            return redirect(url_for('analyze'))
     return render_template('index.html')
+
+
+@app.route("/analyze")
+def analyze():
+    for excel in os.listdir('./flask_upload'):
+        if excel.endswith(".csv"):
+            df = pd.read_csv("flask_upload" + "\\" + excel, sep=",")
+            data = df.describe()
+            os.remove("flask_upload" + "\\" + excel)
+            return data.to_html()
+        elif excel.endswith(".xlsx") or excel.endswith(".xls"):
+            df = pd.read_excel("C:\Users\windows 7\Desktop\Flask_Upload" + "\\" + file,sep=",")
+            data = df.describe()
+            os.remove("C:\Users\windows 7\Desktop\Flask_Upload" + "\\" + file)
+            return data.to_html()
+
 
 @app.route('/post/excel', methods=['POST'])
 def postExcel():
@@ -25,6 +55,7 @@ def postExcel():
     date = request.form.get('date')
     # file1.save('aa.doc') 保存文件
     return json.dumps({'data': '1'})
+
 
 if __name__ == '__main__':
     app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
