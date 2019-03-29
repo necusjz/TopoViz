528505959eaf552809b4303f7b03d1714b1c0aad
necusjz <necusjz@gmail.com>
2019-03-29 10:36:38 +0800

    refine codes based on PEP 8 - hyang

--- backend/app.py
+++ backend/app.py
@@ -3,15 +3,17 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='templates', static_url_path='')
+app = Flask(__name__)
 
 # read config file
 app.config.from_object('config')
 
+
 # upload excel file
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
