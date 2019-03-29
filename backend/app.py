a105f531b304d7d1ef4ea1c0932e3eb57f77aed8
Xin Liu <xin.liu@thoughtworks.com>
2019-03-29 10:47:00 +0800

    update

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 # read config file
 app.config.from_object('config')
