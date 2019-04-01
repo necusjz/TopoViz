dae4f506a07e42a2fd14e7360de803da3196c26b
umarellyh <umarellyh@gmail.com>
2019-04-01 15:25:09 +0800

    fix dictionary bug - hyang

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 SERVER_HOST = 'localhost'
 SERVER_PORT = '8081'
 
-ALLOWED_EXTENSIONS = {['csv', 'xlsx', 'xls']}
+ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'xls'}
 UPLOAD_FOLDER = '../analysis'
 
 DEBUG = False
