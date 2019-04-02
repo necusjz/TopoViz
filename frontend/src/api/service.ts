0d4c320603f9be7d56c2f4be496d571afb72804e
umarelx <umarelx@gmail.com>
2019-04-02 19:20:02 +0800

    unified FE and BE config - hyang

--- frontend/src/api/service.ts
+++ frontend/src/api/service.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 
 const service: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: 'http://localhost:8080/',
   timeout: 150000,
 });
 
