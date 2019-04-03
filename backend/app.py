a246e03ee8a7f6ce2707e34560572a820a27c847
was48i <folklore.hy@gmail.com>
2019-04-03 18:30:44 +0800

    just for test - hyang

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.config.from_object('config')
 CORS(app, resources=r'/*')
-df = []
+df = pd.DataFrame(columns = 'Acked')
 
 
 def allowed_file(filename):
