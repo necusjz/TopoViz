ebd5a53a73b6e5bd2ad223fa350790a22136b010
ICHIGOI7E <ichigoi7e@gmail.com>
2019-04-03 21:41:40 +0800

    fix bugs with analyze route - hyang

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,9 @@ from flask_cors import CORS
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.config.from_object('config')
 CORS(app, resources=r'/*')
-df = pd.DataFrame(columns=['Acked', 'Acked By'])
+
+topo = pd.DataFrame(columns=['PathID', 'NEName'])
+alarm = pd.DataFrame(columns=['Alarm Name', 'Alarm Source'])
 
 
 def allowed_file(filename):
@@ -32,59 +34,62 @@ def upload():
     file2 = request.files['file2']
     date = json.loads(request.form.get('date'))
     if file1 and file2 and date and allowed_file(file1.filename) and \
-            allowed_file(file2.filename):
+       allowed_file(file2.filename):
         filename1 = secure_filename(file1.filename)
         filename2 = secure_filename(file2.filename)
         file1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
         file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
 
-    global df
+    global alarm
+    alarm_ = pd.DataFrame(columns=['Alarm Name', 'Alarm Source'])
     if file2.filename.endswith('.csv'):
-        df = pd.read_csv(file2)
+        alarm_ = pd.read_csv(file2)
     elif file2.filename.endswith('.xlsx') or file2.filename.endswith('xls'):
-        df = pd.read_excel(file2)
+        alarm_ = pd.read_excel(file2)
+    alarm = alarm_[['Alarm Name', 'Alarm Source', 'Vendor', 'First Occurrence',
+                    'Last Occurrence', 'Raw Severity', 'Cleared On', 'Domain',
+                    'RCA Group ID', 'RCA Result', 'RCA Rule Name']]
 
-    a_time = datetime.fromtimestamp(date[0] / 1000)
-    z_time = datetime.fromtimestamp(date[1] / 1000)
-    df['First Occurrence'] = pd.to_datetime(df['First Occurrence'])
-    mask = (a_time <= df['First Occurrence']) & (df['First Occurrence']
-                                                 <= z_time)
-    df = df.loc[mask]
+    a_time = datetime.fromtimestamp(date[0]/1000)
+    z_time = datetime.fromtimestamp(date[1]/1000)
+    alarm['First Occurrence'] = pd.to_datetime(alarm['First Occurrence'])
+    mask = (a_time <= alarm['First Occurrence']) & \
+           (alarm['First Occurrence'] <= z_time)
+    alarm = alarm.loc[mask]
 
     data = dict()
-    data['total_alarm'] = int(df.shape[0])
-    data['p_count'] = int(df[df['RCA Result'] == 1]['RCA Result'].count())
-    data['c_count'] = int(df[df['RCA Result'] == 2]['RCA Result'].count())
-    data['group_count'] = int(pd.Series(df['RCA Group ID']).nunique())
+    data['total_alarm'] = int(alarm.shape[0])
+    data['p_count'] = int(alarm[alarm['RCA Result'] == 1]['RCA Result'].count())
+    data['c_count'] = int(alarm[alarm['RCA Result'] == 2]['RCA Result'].count())
+    data['group_count'] = int(pd.Series(alarm['RCA Group ID']).nunique())
     data['confirmed'] = 0
     data['unconfirmed'] = data['group_count']
-    data['group_id'] = df['RCA Group ID'].drop_duplicates().tolist()
-
+    data['group_id'] = alarm['RCA Group ID'].drop_duplicates().tolist()
     return json.dumps(data)
 
 
 @app.route('/analyze')
 def analyze():
+    global alarm
     group_id = request.args.get('groupId')
     add_condition = request.args.get('addCondition')
     if add_condition:
         add_value = request.args.get('addValue')
         if add_condition == '0':
-            df[df['Domain'] == add_value]
+            alarm = alarm.loc[alarm['Domain'] == add_value]
         if add_condition == '1':
-            df[df['Alarm Name'] == add_value]
+            alarm = alarm.loc[alarm['Alarm Name'] == add_value]
         if add_condition == '2':
-            df[df['RCA Rule Name'] == add_value]
+            alarm = alarm.loc[alarm['RCA Rule Name'] == add_value]
         if add_condition == '3':
-            df[df['RCA Result'] == add_value]
+            alarm = alarm.loc[alarm['RCA Result'] == add_value]
         if add_condition == '4':
-            df[df['RCA Result'] == add_value]
-    df[df['RCA Group ID'] == group_id]
+            alarm = alarm.loc[alarm['RCA Result'] == add_value]
+    alarm = alarm.loc[alarm['RCA Group ID'] == group_id]
 
     data = dict()
     data['topo'] = []
-    data['table'] = df.stack().to_json()
-
+    data['table'] = alarm.to_json(orient='index')
     return json.dumps(data)
 
 
