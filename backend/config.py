d1fe70a515113b3bbb12db8acf9e6d726734c9fe
umarellyh <umarellyh@gmail.com>
2019-04-04 18:30:05 +0800

    for holiday - hyang

--- backend/config.py
+++ backend/config.py
@@ -4,4 +4,10 @@ SERVER_PORT = '8080'
 ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'xls'}
 UPLOAD_FOLDER = 'topo_alarm'
 
+TOPO_COLUMNS = ['PathID', 'NEName', 'Path Hop', 'NEType']
+ALARM_COLUMNS = ['Alarm Name', 'Alarm Source', 'Vendor', 'First Occurrence',
+                 'Last Occurrence', 'Raw Severity', 'Cleared On', 'Domain',
+                 'RCA Group ID', 'RCA Result', 'RCA Rule Name']
+DISTINCT_NUM = 16
+
 DEBUG = False
