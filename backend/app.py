cc142f8d1f12c40b099b30b1e18e2be7dd398760
was48i <folklore.hy@gmail.com>
2019-04-08 16:31:37 +0800

    add interfaces for interval - hyang

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,10 @@
 
 import os
 import json
+import uuid
 import pandas as pd
 
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime
 from flask_cors import CORS
@@ -14,7 +15,7 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 app.config.from_object('config')
 CORS(app, resources=r'/*')
 
-alarm = pd.DataFrame(columns=['Alarm Name', 'Alarm Source'])
+interval = ''
 
 
 def allowed_file(filename):
@@ -22,29 +23,36 @@ def allowed_file(filename):
            app.config['ALLOWED_EXTENSIONS']
 
 
-def save_formatted(file):
+def save_formatted(file, path):
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         if filename.endswith('.csv'):
             df_ = pd.read_csv(file)
             if df_.shape[1] < app.config['DISTINCT_NUM']:
                 df = df_[app.config['TOPO_COLUMNS']]
-                df.to_excel(os.path.join(app.config['UPLOAD_FOLDER'],
-                                         'topo_format.xlsx'), index=False)
+                df.to_excel(path)
             else:
                 df = df_[app.config['ALARM_COLUMNS']]
-                df.to_csv(os.path.join(app.config['UPLOAD_FOLDER'],
-                                       'alarm_format.xlsx'), index=False)
+                df.to_csv(path)
         elif filename.endswith('.xlsx') or filename.endswith('xls'):
             df_ = pd.read_excel(file)
             if df_.shape[1] < app.config['DISTINCT_NUM']:
                 df = df_[app.config['TOPO_COLUMNS']]
-                df.to_excel(os.path.join(app.config['UPLOAD_FOLDER'],
-                                         'topo_format.xlsx'), index=False)
+                df.to_excel(path)
             else:
                 df = df_[app.config['ALARM_COLUMNS']]
-                df.to_excel(os.path.join(app.config['UPLOAD_FOLDER'],
-                                         'alarm_format.xlsx'), index=False)
+                df.to_excel(path)
+
+
+def interval_filter(current_interval, client_id):
+    alarm = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'], client_id,
+                                       'alarm_format.xlsx'))
+    a_time = datetime.fromtimestamp(current_interval[0] / 1000)
+    z_time = datetime.fromtimestamp(current_interval[1] / 1000)
+    alarm['First Occurrence'] = pd.to_datetime(alarm['First Occurrence'])
+    mask = (a_time <= alarm['First Occurrence']) & \
+           (alarm['First Occurrence'] <= z_time)
+    return alarm.loc[mask]
 
 
 @app.route('/', methods=['GET'])
@@ -54,23 +62,27 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # get post request
+    # save formatted files to disk
     file1 = request.files['file1']
     file2 = request.files['file2']
-    date = json.loads(request.form.get('date'))
-    # save formatted files to disk
-    save_formatted(file1)
-    save_formatted(file2)
-    # get time filtered alarm dataframe
-    global alarm
-    alarm = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'],
-                                       'alarm_format.xlsx'))
-    a_time = datetime.fromtimestamp(date[0]/1000)
-    z_time = datetime.fromtimestamp(date[1]/1000)
-    alarm['First Occurrence'] = pd.to_datetime(alarm['First Occurrence'])
-    mask = (a_time <= alarm['First Occurrence']) & \
-           (alarm['First Occurrence'] <= z_time)
-    alarm = alarm.loc[mask]
+    client_id = str(uuid.uuid1())
+    save_path1 = os.path.join(app.config['UPLOAD_FOLDER'] + client_id +
+                              'topo_format.xlsx')
+    save_path2 = os.path.join(app.config['UPLOAD_FOLDER'] + client_id +
+                              'alarm_format.xlsx')
+    save_formatted(file1, save_path1)
+    save_formatted(file2, save_path2)
+    # return unique uuid
+    return jsonify(client_id)
+
+
+@app.route('/interval', methods=['POST'])
+def set_interval():
+    # get interval filtered alarm dataframe
+    global interval
+    interval = json.loads(request.args.get('interval'))
+    client_id = request.headers.get('Client-Id')
+    alarm = interval_filter(interval, client_id)
     # construct json for frontend
     data = dict()
     data['total_alarm'] = int(alarm.shape[0])
@@ -83,9 +95,19 @@ def upload():
     return jsonify(data)
 
 
+@app.route('/reset', methods=['POST'])
+def reset_interval():
+    global interval
+    interval[0] -= 300000
+    interval[1] += 300000
+    return redirect(url_for('analyze'))
+
+
 @app.route('/analyze')
 def analyze():
-    global alarm
+    global interval
+    client_id = request.headers.get('Client-Id')
+    alarm = interval_filter(interval, client_id)
     group_id = request.args.get('groupId')
     alarm = alarm.loc[alarm['RCA Group ID'] == group_id]
 
