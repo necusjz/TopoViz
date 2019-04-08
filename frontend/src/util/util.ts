a2c7740fe7755402f5b0fe4bf58aeb58d8b0b5da
Alanbaba2018 <alan19940116@gmail.com>
2019-04-08 10:17:19 +0800

    update frontend

--- frontend/src/util/util.ts
+++ frontend/src/util/util.ts
@@ -15,3 +15,30 @@ export function throttle(fn: any, delay: number = 50, isDebounce: boolean = fals
       }
     }
   }
+
+  export function generateUUID(): string {
+    let d = new Date().getTime();
+    if (window.performance && typeof window.performance.now === "function") {
+        d += performance.now(); //use high-precision timer if available
+    }
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        const r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
+
+function formatSingleNumber(num: number): string {
+  return num < 10 ? '0' + num : num.toString();
+}
+export function generateDateByTimestamp(timeStamp: number): string {
+  const date = new Date(timeStamp);
+  const year: number = date.getFullYear();
+  const month: number = date.getMonth() + 1;
+  const day: number = date.getDate();
+  const h: number = date.getHours();
+  const m: number = date.getMinutes();
+  const s: number = date.getSeconds();
+  return `${year}-${formatSingleNumber(month)}-${formatSingleNumber(day)} ${formatSingleNumber(h)}:${formatSingleNumber(m)}:${formatSingleNumber(s)}`
+}
