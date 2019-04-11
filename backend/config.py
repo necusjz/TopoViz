655779ddfe7420def96de65fa6fc666e3dd46d44
umarelx <umarelx@gmail.com>
2019-04-11 16:48:13 +0800

    add columns mapping - hyang

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,20 @@
 SERVER_HOST = 'localhost'
 SERVER_PORT = '8080'
 
-ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'xls'}
+ALLOWED_EXTENSIONS = {'xlsx', 'xls', 'csv'}
+ALARM_FILE = 'alarm_format.xlsx'
+TOPO_FILE = 'topo_format.xlsx'
 UPLOAD_FOLDER = 'temp'
 
-TOPO_COLUMNS = ['PathID', 'NEName', 'Path Hop', 'NEType']
 ALARM_COLUMNS = ['Alarm Name', 'Alarm Source', 'Vendor', 'First Occurrence',
                  'Last Occurrence', 'Raw Severity', 'Cleared On', 'Domain',
                  'RCA Group ID', 'RCA Result', 'RCA Rule Name']
+ALARM_MAPPING = ['AlarmName', 'AlarmSource', 'Vendor', 'First', 'Last', 'Level',
+                 'Clear', 'Domain', 'GroupID', 'RCAResult', 'RuleName']
+
+TOPO_COLUMNS = ['PathID', 'NEName', 'Path Hop', 'NEType']
+TOPO_MAPPING = ['PathID', 'NEName', 'PathHop', 'NEType']
+
 DISTINCT_NUM = 16
 
 DEBUG = False
