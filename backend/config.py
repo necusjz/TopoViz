dfdc25a78f5a1cf1f9f661ee7e1785c79f2cee2d
umarellyh <umarellyh@gmail.com>
2019-04-12 00:34:11 +0800

    add locate route - hyang

--- backend/config.py
+++ backend/config.py
@@ -1,20 +1,20 @@
 SERVER_HOST = 'localhost'
 SERVER_PORT = '8080'
 
-ALLOWED_EXTENSIONS = {'xlsx', 'xls', 'csv'}
 UPLOAD_FOLDER = 'temp'
+ALLOWED_EXTENSIONS = {'xlsx', 'xls', 'csv'}
 
-ALARM_FILE = 'alarm_format.xlsx'
 TOPO_FILE = 'topo_format.xlsx'
+ALARM_FILE = 'alarm_format.xlsx'
+
+TOPO_COLUMNS = ['PathID', 'NEName', 'Path Hop', 'NEType']
+TOPO_MAPPING = ['PathId', 'NEName', 'PathHop', 'NEType']
 
 ALARM_COLUMNS = ['Alarm Name', 'Alarm Source', 'Vendor', 'First Occurrence',
                  'Last Occurrence', 'Raw Severity', 'Cleared On', 'Domain',
                  'RCA Group ID', 'RCA Result', 'RCA Rule Name']
-ALARM_MAPPING = ['AlarmName', 'AlarmSource', 'Vendor', 'First', 'Last', 'Level',
-                 'Clear', 'Domain', 'GroupID', 'RCAResult', 'RuleName']
-
-TOPO_COLUMNS = ['PathID', 'NEName', 'Path Hop', 'NEType']
-TOPO_MAPPING = ['PathID', 'NEName', 'PathHop', 'NEType']
+ALARM_MAPPING = ['AlarmName', 'AlarmSource', 'Vendor', 'First', 'Last',
+                 'Level', 'Clear', 'Domain', 'GroupId', 'RcaResult', 'RuleName']
 
 DISTINCT_NUM = 16
 
