edcc4c0d67e7aca848a65ae0cee528945f05c328
Alanbaba2018 <alan19940116@gmail.com>
2019-04-19 12:36:35 +0800

    fix IE problem

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './styles/global.scss';
 import 'nprogress/nprogress.css';
+import '@/util/brower';
 
 Vue.use(ElementUI);
 
