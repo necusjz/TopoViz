5d97fed8107a764eaf074696cc6a29b215713507
Alanbaba2018 <alan19940116@gmail.com>
2019-04-19 12:36:35 +0800

    fix IE problem

--- frontend/src/util/brower.ts
+++ frontend/src/util/brower.ts
@@ -1,9 +1,13 @@
-if (Array.prototype.flat === undefined) {
+// if (Array.prototype.flat === undefined) {
   Array.prototype.flat = function() {
     const arr: any = [];
     for (let i = 0; i < this.length; i++) {
-
+      if (Array.isArray(this[i])) {
+        arr.push(...this[i]);
+      } else {
+        arr.push(this[i]);
+      }
     }
     return arr;
   }
-}
+// }
