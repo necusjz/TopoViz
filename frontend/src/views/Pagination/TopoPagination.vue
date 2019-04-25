aca979118615624fc91da5cc23212bfb79aa2b89
lonelyWebDev <alan19940116@gmail.com>
2019-04-25 14:18:55 +0800

    update frontend

--- frontend/src/views/Pagination/TopoPagination.vue
+++ frontend/src/views/Pagination/TopoPagination.vue
@@ -13,7 +13,8 @@
 <script lang="ts">
 import { Component, Vue, Provide, Watch } from "vue-property-decorator";
 import { State } from 'vuex-class';
-import { AlarmData } from '@/types/type';
+import { AlarmData, EventType } from '@/types/type';
+import bus from '@/util/bus';
 
 @Component
 export default class TopoTablePagination extends Vue {
@@ -25,6 +26,7 @@ export default class TopoTablePagination extends Vue {
     @State((state) => state.app.selectAlarm) private selectAlarm!: string;
     @State((state) => state.app.pageData) private pageData!: AlarmData[];
     @State((state) => state.app.alarmDatas) private alarmDatas!: AlarmData[];
+    @State((state) => state.app.needSave) private needSave!: boolean;
     @Watch('tableData')
     public watchTableData(val: AlarmData[]) {
       this.initPagination();
@@ -41,16 +43,38 @@ export default class TopoTablePagination extends Vue {
       this.submitPageData();
     }
     public handleSizeChange(size: number) {
-      this.pageSize = size;
-      if (this.currentPage > Math.ceil(this.totalSize / size)) {
-        this.currentPage = 1;
-      } else {
-        this.submitPageData();
+      if (this.needSave) {
+        bus.$emit(EventType.ERRORVISIBLE, {
+          title: '错误提示',
+          content: '<p>当前结果未保存，您确定要离开吗？</p>',
+          confirmCallback: () => {
+            this.$store.commit('SET_NEEDSAVE', false);
+            this.pageSize = size;
+            if (this.currentPage > Math.ceil(this.totalSize / size)) {
+              this.currentPage = 1;
+            } else {
+              this.submitPageData();
+            }
+          },
+          saveCallback: () => {
+            bus.$emit(EventType.SAVEDATA);
+          }
+        });
       }
     }
     public handleCurrentChange(page: number) {
-      this.currentPage = page;
-      this.submitPageData();
+      bus.$emit(EventType.ERRORVISIBLE, {
+        title: '错误提示',
+        content: '<p>当前结果未保存，您确定要离开吗？</p>',
+        confirmCallback: () => {
+          this.$store.commit('SET_NEEDSAVE', false);
+          this.currentPage = page;
+          this.submitPageData();
+        },
+        saveCallback: () => {
+          bus.$emit(EventType.SAVEDATA);
+        }
+      });
     }
     public submitPageData() {
       const currentPageData: AlarmData[] = this.tableData.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize);
