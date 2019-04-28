2660e262d1a327023d3e6f68d579d7086862d0c5
umarelx <umarelx@gmail.com>
2019-04-28 11:20:30 +0800

    inquire to locate - hyang

--- frontend/src/util/config.ts
+++ frontend/src/util/config.ts
@@ -1,28 +1,27 @@
 import { Rules } from '../types/type';
 
 const ruleOptions = [{
-    label: `按厂商查询`,
+    label: `按厂商定位`,
     value: Rules.company,
     children: []
 },{
-    label: `按告警名称查询`,
+    label: `按告警名称定位`,
     value: Rules.alarmName,
     children: []
 },{
-    label: `按 RCA 规则查询`,
+    label: `按 RCA 规则定位`,
     value: Rules.rcaReg,
     children: []
 },{
-    label: `按 P 告警查询`,
+    label: `按 P 告警定位`,
     value: Rules.pAlarm,
     children: []
 },{
-    label: `按 C 告警查询`,
+    label: `按 C 告警定位`,
     value: Rules.cAlarm,
     children: []
 }];
 
-// const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'http://10.40.49.171:5000/';
 const baseUrl = 'http://localhost:5000/';
 
 export {
