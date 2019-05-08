a747412d688a2f747ac26da2f2a999123e01bf8b
Alanbaba2018 <1294284048@qq.com>
2019-05-08 16:32:58 +0800

    修改未知告警

--- frontend/src/views/Title/Index.vue
+++ frontend/src/views/Title/Index.vue
@@ -43,6 +43,7 @@ export default class Title extends Vue {
   }
   public goBack() {
     this.$store.commit('SET_ISCHECKSTATICS', false);
+    this.$store.commit('SET_ISCHECKNONE', false);
   }
 }
 </script>
