5f33d07766763a388f4d7b95cba515c3123bf9ad
umarelx <umarelx@gmail.com>
2019-05-09 16:52:45 +0800

    for multiple languages - hyang

--- frontend/src/i18n/langs/cn.ts
+++ frontend/src/i18n/langs/cn.ts
@@ -7,6 +7,14 @@ const cn = {
     importTopoData: '导入 TOPO 数据',
     importRCAData: '导入 RCA 结果',
     rcaResultStatics: 'RCA 结果汇总',
+    totalAlarm: '总告警',
+    pAlarm: 'P 告警',
+    cAlarm: 'C 告警',
+    xAlarm: '未知告警',
+    groupNumber: '组数量',
+    confirmed: '已确认组',
+    unconfirmed: '未确认组',
+    accuracy: 'RCA 准确率'
   },
   ...zhLocale
 }
