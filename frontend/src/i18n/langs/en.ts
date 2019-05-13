6d4882fce4b4a82223b23f6da0f2c11478aac934
was48i <folklore.hy@gmail.com>
2019-05-13 15:03:44 +0800

    some translation - hyang

--- frontend/src/i18n/langs/en.ts
+++ frontend/src/i18n/langs/en.ts
@@ -3,18 +3,22 @@ import zhLocale from 'element-ui/lib/locale/lang/en';
 const en = {
   lang: {
     upload: 'Upload',
-    export: 'Export',
+    export: 'Export Data',
     importTopoData: 'Import TOPO',
     importRCAData: 'Import RCA',
     rcaResultStatics: 'Summary',
     totalAlarm: 'Total Alarm',
     pAlarm: 'P Alarm',
     cAlarm: 'C Alarm',
-    xAlarm: 'Unknown',
+    xAlarm: 'Unknown Alarm',
     groupNumber: 'Group Number',
     confirmed: 'Confirmed',
     unconfirmed: 'Unconfirmed',
     accuracy: 'Accuracy',
+    noData: 'No Data',
+    noDataMsg: 'You have not confirmed the data, go to "unconfirmed" to see it.',
+    knownAlarm: 'Known Alarm',
+    unknownAlarm: 'Unknown Alarm',
   },
   ...zhLocale
 }
