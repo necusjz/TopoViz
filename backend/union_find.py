6f109c0140f5c916b05dd5b37f056de9cf600206
snlndod <snlndod@gmail.com>
2019-06-04 16:16:48 +0800

    refine codes - hyang

--- backend/union_find.py
+++ backend/union_find.py
@@ -4,33 +4,30 @@
 
 class UnionFind(object):
     id = []
-    count = 0
     sz = []
+    count = 0
 
     def __init__(self, n):
-        self.count = n
         i = 0
+        self.count = n
         while i < n:
             self.id.append(i)
             self.sz.append(1)
             i += 1
 
-    def connected(self, p, q):
-        return self.find(p) == self.find(q)
+    def find(self, x):
+        while x != self.id[x]:
+            x = self.id[x]
+        return x
 
-    def find(self, p):
-        while p != self.id[p]:
-            p = self.id[p]
-        return p
-
-    def union(self, p, q):
-        idp = self.find(p)
-        idq = self.find(q)
-        if not self.connected(p, q):
-            if self.sz[idp] < self.sz[idq]:
-                self.id[idp] = idq
-                self.sz[idq] += self.sz[idp]
+    def unite(self, x, y):
+        idx = self.find(x)
+        idy = self.find(y)
+        if not self.find(x) == self.find(y):
+            if self.sz[idx] < self.sz[idy]:
+                self.id[idx] = idy
+                self.sz[idy] += self.sz[idx]
             else:
-                self.id[idq] = idp
-                self.sz[idp] += self.sz[idq]
+                self.id[idy] = idx
+                self.sz[idx] += self.sz[idy]
             self.count -= 1
